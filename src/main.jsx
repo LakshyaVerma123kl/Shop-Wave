273b4990cd8d02a2421f80be038873cd30de937b
LakshyaVerma123kl <lakshya123kl@gmail.com>
2024-08-30 10:41:37 +0530

    new2

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client"; // Updated import for React 18
 import "./index.css";
 import App from "./App.jsx"; // Import the App component
 
-const root = ReactDOM.createRoot(document.getElementById("root")); // Create a root for React 18
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Shop-Wave/", // Set this to your repository name
+  base: "/Shop-Wave/", // Correctly set to your repository name
   plugins: [react()],
 });
