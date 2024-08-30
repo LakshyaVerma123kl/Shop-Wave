0e2d374d911ebd111669d8b5d815ab3ead4c991c
LakshyaVerma123kl <lakshya123kl@gmail.com>
2024-08-30 11:15:39 +0530

    new4

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Shop-Wave/", // Ensure this matches your GitHub repository name
+  base: "/Shop-Wave/",
   plugins: [react()],
 });
